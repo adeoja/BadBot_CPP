8ae47c761b8e9c7a36e543beed5378c14e1ab7d8
adeoja <cadeoja@gmail.com>
2025-04-11 15:42:43 -0500

    Created drone and blaster beam cpp files

---
+++ Source/BadBot_CPP/CPP_BlasterBeam.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CPP_BlasterBeam.h"
+
+// Sets default values
+ACPP_BlasterBeam::ACPP_BlasterBeam()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ACPP_BlasterBeam::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACPP_BlasterBeam::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
